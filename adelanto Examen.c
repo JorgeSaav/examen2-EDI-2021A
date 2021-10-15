32a7ff5f0327c6e7801792bb725207dc8ad46f37
Jorge Ramirez <89396895+JorgeSaav@users.noreply.github.com>
2021-10-15 09:20:59 -0500

    Add files via upload

---
+++ adelanto Examen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+	
+}
+
+int CapturaDatos
+
+int pideNombre()
+{
+	int Nombre[i];
+	
+	printf("Dame el nombre del usuario");
+	scanf("%s", &Nombre[i])
+}
+
+int pideClave()
+{
+	int c;
+	printf("Dame la clave del usuario: ");
+	scanf("%d", &c);
+}
+
+int pideEyS()
+{
+	int i, j, r, c;
+	float arr[][]
+	
+	for(i=0; i<r; i++)
+		for(j=0; j<c; j++)
+		{
+			for(j=0)
+			{
+				printf("Dame la estatura")
+				scanf("%f", &arr[i][j];
+			}
+			else
+			{
+				printf("Dame tu sueldo")
+				scanf("%f", &arr[i][j]);
+			}
+		}
+}
